89e6b4a44159c44c0cec140438dc303b925eb20f
isabelaTV <isabellactv96@gmail.com>
2024-10-26 16:21:14 -0500

    se modifica estilos de la pagina de productos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,38 +2,7 @@ import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
 import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
-
-
-
-// const Products = () => {
-//   return (
-//     <>
-//     <header>
-//       <Navbar/>
-//     </header>
-//     <main className="container">
-//           <div className="product-grid">
-//             {listProducts.map(product => (
-//               <div key={product.id} className="product-containner">
-//                 <img src={product.img} alt={product.nombre} className="product-image" />
-//                 <h2 className="product-name">{product.nombre}</h2>
-//                 <p className="product-price">{product.precio}</p>
-//                 <Link to={`/productDetails?id=${product.id}`}>Ver Detalles</Link>
-//               </div>
-//             ))}
-//           </div>
-//       </main>
-//       <footer>
-//         <p>© BellArt 2020- 2024 | Todos los derechos reservados</p>
-//       </footer>
-//     </>
-//   )
-// }
-
-// export default Products;
-
-
-
+import "./product.scss"
 
 const Products = () => {
   const listProducts = [
@@ -55,7 +24,26 @@ const Products = () => {
       nombre: 'mueble',
       precio: '$4000',
       img: "https://img.freepik.com/foto-gratis/hermosa-composicion-collage-vintage_23-2149479770.jpg?t=st=1728552468~exp=1728556068~hmac=39a0268679825cfe9a0e9d8201882001d13a75c56832abb040527d07969e9ce3&w=360"
-  }
+  },
+  {
+    id: 4,
+    nombre: "sofa",
+    precio: "$4000",
+    img: "https://img.freepik.com/foto-gratis/ojos-brillaron-colores-vibrantes-hojas-otono-ia-generativa_8829-2921.jpg?t=st=1728552285~exp=1728555885~hmac=45423d0417945c155232ded4fa24bb1c6b5469616fc16117751a9d884a0f4586&w=360",
+    descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste iusto perferendis corrupti, debitis possimus esse maxime saepe unde. Nobis obcaecati magni reiciendis vel incidunt corporis ratione doloremque, esse quidem rerum."
+  },
+  {
+    id: 5,
+    nombre: 'cama',
+    precio: '$8000',
+    img: "https://img.freepik.com/foto-gratis/hermosa-composicion-collage-vintage_23-2149479769.jpg?t=st=1728552366~exp=1728555966~hmac=b80efb1d48b0a8d9deb130f2a2d4f7fb36e7de65485c687f6fd6c348941cb786&w=360"
+  },
+  {
+    id: 6,
+    nombre: 'mueble',
+    precio: '$4000',
+    img: "https://img.freepik.com/foto-gratis/hermosa-composicion-collage-vintage_23-2149479770.jpg?t=st=1728552468~exp=1728556068~hmac=39a0268679825cfe9a0e9d8201882001d13a75c56832abb040527d07969e9ce3&w=360"
+}
   ];
 
   return (
@@ -63,11 +51,14 @@ const Products = () => {
       <header>
         <Navbar/>
       </header>
-        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      <main className="container">
+        <h2>Arte digital</h2>
+        <p>Step away from traditional art forms and explore the possibilities of digital art. Discover graphic, illustrative and typographic art.</p>
+        <div className="productGrid">
           {listProducts.map((product) => (
-          <Link to={`/productDetails?id=${product.id}` } key={product.id}>
-          <Card shadow="sm" isPressable >
-            <CardBody className="overflow-visible p-0">
+          <Link className="link-product-detail" to={`/productDetails?id=${product.id}` } key={product.id}>
+          <Card shadow="sm" isPressable className="productContainer">
+            <CardBody className="overflow-visible p-0 cardImg">
               <Image
                 shadow="sm"
                 radius="lg"
@@ -84,7 +75,11 @@ const Products = () => {
           </Card>
         </Link>
         ))}
-        </div>    
+        </div>
+        </main>    
+      <footer>
+        <p>© BellArt 2020- 2024 | Todos los derechos reservados</p>
+      </footer>
     </>
 
   );
